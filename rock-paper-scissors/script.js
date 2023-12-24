78a07e6b8add932b19663ba16c969584eda87678
jen67 <amakrigift2000@gmail.com>
2023-12-24 09:02:00 +0100

    started this project

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const themeContainer = document.querySelector(".theme-toggler");
+const darkThemeToggler = document.querySelector(".dark-toggle");
+const lightThemeToggler = document.querySelector(".light-toggle")
+
+themeContainer.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    darkThemeToggler.style.display = "block ";
+    lightThemeToggler.style.display = "none";
+    
+    if (body.classList.contains("dark-mode")) {
+        darkThemeToggler.style.display = "none";
+        lightThemeToggler.style.display = "block"; 
+    }
+});
