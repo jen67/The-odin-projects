eddb9881c1bfe9d32d83b9bf702e64cf8997f107
jen67 <amakrigift2000@gmail.com>
2023-12-27 16:58:36 +0100

    added some codes

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -7,24 +7,44 @@ const gameContainer = document.querySelector(".game-container");
 const gameRulesContainer = document.querySelector(".about-game-modal");
 const closeBtn = document.querySelector(".close");
 
-// handles the game rules modal
-gameRules.addEventListener("click", () => {
-    gameContainer.style.display = "none";
-    gameRulesContainer.style.display = "block";
+// Load the theme preference from localStorage on page load
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    body.classList.add(savedTheme);
+    updateThemeToggleDisplay();
+  }
 });
 
-closeBtn.addEventListener("click", () => {
-  gameRulesContainer.style.display = "none";
-  gameContainer.style.display = "block";
-});
-
-themeContainer.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
+// Function to update the display of theme toggles
+function updateThemeToggleDisplay() {
   darkThemeToggler.style.display = body.classList.contains("dark-mode")
     ? "none"
     : "block";
   lightThemeToggler.style.display = body.classList.contains("dark-mode")
     ? "block"
     : "none";
+}
+
+// Function to toggle the theme
+function toggleTheme() {
+  body.classList.toggle("dark-mode");
+  updateThemeToggleDisplay();
+
+  // Save the current theme preference to localStorage
+  const currentTheme = body.classList.contains("dark-mode") ? "dark-mode" : "";
+  localStorage.setItem("theme", currentTheme);
+}
+
+// Event listeners
+themeContainer.addEventListener("click", toggleTheme);
+
+gameRules.addEventListener("click", () => {
+  gameContainer.style.display = "none";
+  gameRulesContainer.style.display = "block";
 });
 
+closeBtn.addEventListener("click", () => {
+  gameRulesContainer.style.display = "none";
+  gameContainer.style.display = "block";
+});
